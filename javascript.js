53aed8ba3f154d1058c49d22e0c313efed1080f7
Maaz <maazshaikh4559@gmail.com>
2022-04-10 01:14:49 +0530

    Function for playing a round (playround) created

--- javascript.js
+++ javascript.js
@@ -1,1 +1,66 @@
-console.log('Hello World!')
+function computerPlay() {
+let i = Math.floor(Math.random() * 3);
+    let returnValue = "";
+    // if 0, it's rock
+    if (i === 0) {
+        returnValue = "rock";
+    }
+    // if 1 it's paper
+    else if (i === 1) {
+        returnValue = "paper";
+    }
+    // if 2 (or 3) it's scissors
+    else {
+        returnValue = "scissors";
+    }
+    // return the selection via a string
+    return returnValue;
+}
+// get user's play and sanatise
+function userPlay() {
+    return playerSelection = prompt("Please choose Rock, paper or scissors", "rock").toLowerCase
+}
+
+// Function will play a single round of rock, paper, scissors. Requires user input. 
+function playRound(playerSelection , computerSelection){
+    let result = ""
+    if (playerSelection === computerSelection){
+        result =  "tied"
+    }
+    else if (playerselection === "rock"){
+        if (computerSelection === "scissors"){
+              result = "won"
+        }
+        else if (computerSelection === "paper"){
+            result = "lost"
+        }
+    }
+    else if (playerSelection === "paper"){
+        if (computerSelection === "rock"){
+            result = "won"
+        }
+        else if (computerSelection === "scissors"){
+            result = "lost"
+        }
+    }
+    else if (playerSelection === "scissors"){
+        if (computerSelection === "rock") {
+            result = "lost"
+        }
+        else if (computerSelection === "paper") {
+            result = "won"
+        }
+    }
+    
+    //checking the result
+    if (result === "tied") {
+         return [result, `You both have chosen ${playerSelection}! It's a tie.`];
+    }
+    else if (result === "lost") {
+        return [result, `You lose! ${computerSelection} beats ${playerSelection}.`];
+    }
+    else if (result === "won") {
+        return [result, `You win! ${playerSelection} beats ${computerSelection}.`];
+    }
+}
+
